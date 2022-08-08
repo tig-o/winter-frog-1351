92abf7a9ad2fa4691c1cba3a8a713f2ba32ca4be
Thiago Silveira <thiago1992@yahoo.com>
2022-08-08 10:54:52 -0600

    Test: displays a link to remove plant from a plot

--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'plots index page', type: :feature do
-    it 'displays list of all plot numbers and plant of specific plots names' do
+    it 'displays list of all plot numbers and plant of specific plots' do
         sun_garden = Garden.create!(name: "Sun Garden", organic: true)
 
         plot1 = Plot.create!(number: 1, size: "Small", direction: "North", garden: sun_garden)
@@ -53,6 +53,50 @@ RSpec.describe 'plots index page', type: :feature do
             expect(page).to_not have_content("Pineapple")
             expect(page).to_not have_content("Tomato")
         end
-        save_and_open_page
     end
+
+    it 'displays a link to remove plant from a plot' do
+        sun_garden = Garden.create!(name: "Sun Garden", organic: true)
+
+        plot1 = Plot.create!(number: 1, size: "Small", direction: "North", garden: sun_garden)
+        plot2 = Plot.create!(number: 2, size: "Medium", direction: "East", garden: sun_garden)
+        plot3 = Plot.create!(number: 3, size: "Large", direction: "South", garden: sun_garden)
+
+        pineapple = Plant.create!(name: "Pineapple", description: "Low maintenance", days_to_harvest: 30)
+        watermelon = Plant.create!(name: "Watermelon", description: "Medium maintenance", days_to_harvest: 60)
+        kiwi = Plant.create!(name: "Kiwi", description: "High maintenance", days_to_harvest: 90)
+        tomato = Plant.create!(name: "Tomato", description: "Low maintenance", days_to_harvest: 30)
+        onion = Plant.create!(name: "Onion", description: "Medium maintenance", days_to_harvest: 60)
+        squash = Plant.create!(name: "Squash", description: "High maintenance", days_to_harvest: 90)
+        pepper = Plant.create!(name: "Bell Pepper", description: "High maintenance", days_to_harvest: 90)
+
+        PlotPlant.create!(plot: plot1, plant: pineapple)
+        PlotPlant.create!(plot: plot1, plant: watermelon)
+        PlotPlant.create!(plot: plot1, plant: kiwi)
+        PlotPlant.create!(plot: plot2, plant: tomato)
+        PlotPlant.create!(plot: plot2, plant: onion)
+        PlotPlant.create!(plot: plot3, plant: squash)
+        PlotPlant.create!(plot: plot3, plant: pepper)
+
+        visit "/plots"
+
+        within "#plot#{plot1.id}" do
+            expect(page).to have_content("Pineapple")
+            expect(page).to have_content("Watermelon")
+            expect(page).to have_content("Kiwi")
+            expect(page).to have_link("Remove Plant")
+
+            within "#plant#{pineapple.id}" do
+                click_on "Remove Plant"
+            end
+
+            expect(page).to_not have_content("Pineapple")
+            expect(page).to have_content("Watermelon")
+            expect(page).to have_content("Kiwi")
+            expect(pineapple).to be_a(Plant)
+        end
+
+        expect(current_path).to eq("/plots")
+    end
+    
 end
