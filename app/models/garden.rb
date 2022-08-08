dca46ca54225ce9cd54c2686f097822452d3e9a6
Thiago Silveira <thiago1992@yahoo.com>
2022-08-08 11:41:57 -0600

    Feature: garden instance method unique plant list at #below_one_hundred_harvest

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,3 +1,11 @@
 class Garden < ApplicationRecord
   has_many :plots
+
+  def below_one_hundred_harvest
+    plots.joins(plants: :plot_plants)
+      .where('plants.days_to_harvest <= ?', 100)
+      .select('plants.*')
+      .distinct
+      .order(:name)
+  end
 end
